eeedfa7cdcf19de884f3f7d75fc8c8f938292e3d
Rohit <rohitkumar@caplineservices.com>
2025-12-22 23:21:47 +0530

    db update

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI || "mongodb+srv://rohit123:mn%40%40%24db@cluster0.lstnt4r.mongodb.net/dsa_topic?retryWrites=true&w=majority&appName=Cluster0");
     console.log("MongoDB Connected");
 };
 
